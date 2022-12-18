78252722d645da0f47b7d3ddb8f423453609e8b4
progIfe <ifeoluwasamson90@gmail.com>
2022-12-18 14:52:55 +0100

    Created Food and Consume models

--- food/views.py
+++ food/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Food
 
 # Create your views here.
+
+
+def index(request):
+    foods = Food.objects.all()
+    context = {
+        'foods': foods,
+    }
+
+    return render(request, 'index.html', context)
